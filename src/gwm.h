7e992dbe5b24212d441a403be668909cd1023e57
gsm <406643764@qq.com>
2022-01-03 13:33:05 +0800

    爲了實現操作中心的功能，創建了CMD_CENTER_BUTTON_N、CMD_CENTER_BUTTON_INDEX、IS_CMD_CENTER_BUTTON宏和Cmd_center類型以及create_cmd_center、update_cmd_center_button_text、hint_enter_cmd_center_button、hint_leave_cmd_center_button、open_cmd_center函數聲明，修改了Widget_type、WM類型；爲了實現光標進入窗口時聚焦窗口的功能，創建了Area_type類型和focus_clicked_client、choose_client、toggle_focus_mode函數聲明，刪除了choose_client_in_preview、key_choose_client函數聲明，修改了Taskbar、WM類型；爲了實現任務欄隨狀態區域文字長度變化而調整各區域的寬度的功能，創建了create_icon_area函數聲明。

--- src/gwm.h
+++ src/gwm.h
@@ -1,6 +1,6 @@
 /* *************************************************************************
  *     gwm.h：與gwm.c相應的頭文件。
- *     版權 (C) 2020 gsm <406643764@qq.com>
+ *     版權 (C) 2020-2022 gsm <406643764@qq.com>
  *     本程序為自由軟件：你可以依據自由軟件基金會所發布的第三版或更高版本的
  * GNU通用公共許可證重新發布、修改本程序。
  *     雖然基于使用目的而發布本程序，但不負任何擔保責任，亦不包含適銷性或特
@@ -33,34 +33,40 @@
 
 #define TITLE_BUTTON_N ARRAY_NUM(TITLE_BUTTON_TEXT)
 #define TASKBAR_BUTTON_N ARRAY_NUM(TASKBAR_BUTTON_TEXT)
+#define CMD_CENTER_BUTTON_N ARRAY_NUM(CMD_CENTER_BUTTON_TEXT)
 
 #define BUTTON_MASK (ButtonPressMask|ButtonReleaseMask)
 #define POINTER_MASK (BUTTON_MASK|ButtonMotionMask)
 #define CROSSING_MASK (EnterWindowMask|LeaveWindowMask)
 #define ROOT_EVENT_MASK (SubstructureRedirectMask|SubstructureNotifyMask| \
     PropertyChangeMask|POINTER_MASK|PointerMotionMask|ExposureMask|CROSSING_MASK)
-#define TASKBAR_EVENT_MASK (SubstructureRedirectMask|SubstructureNotifyMask|\
-    ExposureMask)
 #define BUTTON_EVENT_MASK (ButtonPressMask|ExposureMask|CROSSING_MASK)
 #define FRAME_EVENT_MASK (SubstructureRedirectMask|SubstructureNotifyMask| \
     ExposureMask|ButtonPressMask|CROSSING_MASK)
 #define TITLE_AREA_EVENT_MASK (ButtonPressMask|ExposureMask|CROSSING_MASK)
-#define STATUS_AREA_EVENT_MASK (ButtonPressMask|ExposureMask|CROSSING_MASK)
 
 #define TITLE_BUTTON_INDEX(type) ((type)-TITLE_BUTTON_BEGIN)
-#define TITLE_BUTTON_TYPE(index) (TITLE_BUTTON_BEGIN+(index))
 #define IS_TITLE_BUTTON(type) \
     ((type)>=TITLE_BUTTON_BEGIN && (type)<=TITLE_BUTTON_END)
 
 #define TASKBAR_BUTTON_INDEX(type) ((type)-TASKBAR_BUTTON_BEGIN)
-#define TASKBAR_BUTTON_TYPE(index) (TASKBAR_BUTTON_BEGIN-(index))
 #define IS_TASKBAR_BUTTON(type) \
     ((type)>=TASKBAR_BUTTON_BEGIN && (type)<=TASKBAR_BUTTON_END)
+
+#define CMD_CENTER_BUTTON_INDEX(type) ((type)-CMD_CENTER_BUTTON_BEGIN)
+#define IS_CMD_CENTER_BUTTON(type) \
+    ((type)>=CMD_CENTER_BUTTON_BEGIN && (type)<=CMD_CENTER_BUTTON_END)
+
+enum focus_mode_tag
+{
+    ENTER_FOCUS, CLICK_FOCUS,
+};
+typedef enum focus_mode_tag Focus_mode;
 
 enum area_type_tag
 {
      MAIN_AREA, SECOND_AREA, FIXED_AREA, FLOATING_AREA, ICONIFY_AREA,
-     AREA_TYPE_N, PREV_AREA,
+     AREA_TYPE_N, PREV_AREA, ROOT_AREA,
 };
 typedef enum area_type_tag Area_type;
 
@@ -71,8 +77,16 @@ enum widget_type_tag
     MAIN_BUTTON, SECOND_BUTTON, FIXED_BUTTON, FLOAT_BUTTON,
     ICON_BUTTON, MAX_BUTTON, CLOSE_BUTTON,
     FULL_BUTTON, PREVIEW_BUTTON, STACK_BUTTON, TILE_BUTTON, DESKTOP_BUTTON,
+    CMD_CENTER_BUTTON,
+    HELP_BUTTON, FILE_BUTTON, TERM_BUTTON, BROWSER_BUTTON, 
+    VOLUME_DOWN_BUTTON, VOLUME_UP_BUTTON, VOLUME_MAX_BUTTON, VOLUME_TOGGLE_BUTTON,
+    MAIN_NEW_BUTTON, SEC_NEW_BUTTON, FIX_NEW_BUTTON, FLOAT_NEW_BUTTON,
+    ICON_NEW_BUTTON, N_MAIN_UP_BUTTON, N_MAIN_DOWN_BUTTON, FOCUS_MODE_BUTTON,
+    QUIT_WM_BUTTON, LOGOUT_BUTTON, REBOOT_BUTTON, POWEROFF_BUTTON,
+    RUN_BUTTON,
     TITLE_BUTTON_BEGIN=MAIN_BUTTON, TITLE_BUTTON_END=CLOSE_BUTTON,
-    TASKBAR_BUTTON_BEGIN=FULL_BUTTON, TASKBAR_BUTTON_END=DESKTOP_BUTTON,
+    TASKBAR_BUTTON_BEGIN=FULL_BUTTON, TASKBAR_BUTTON_END=CMD_CENTER_BUTTON,
+    CMD_CENTER_BUTTON_BEGIN=HELP_BUTTON, CMD_CENTER_BUTTON_END=RUN_BUTTON,
 };
 typedef enum widget_type_tag Widget_type;
 
@@ -123,12 +137,18 @@ typedef enum pointer_act_tag Pointer_act;
 
 struct taskbar_tag
 {
-    Window win, buttons[TASKBAR_BUTTON_N], status_area;
+    Window win, buttons[TASKBAR_BUTTON_N], icon_area, status_area;
     int x, y;
-    unsigned int w, h;
+    unsigned int w, h, status_area_w;
     char *status_text;
 };
 typedef struct taskbar_tag Taskbar;
+
+struct cmd_center_tag
+{
+    Window win, buttons[CMD_CENTER_BUTTON_N];
+};
+typedef struct cmd_center_tag Cmd_center;
 
 struct wm_tag
 {
@@ -143,9 +163,11 @@ struct wm_tag
         n_main_max; /* 主區域可容納的clients數量 */
     Layout cur_layout, prev_layout;
     Area_type default_area_type;
+    Focus_mode focus_mode;
     XFontSet font_set;
     Cursor cursors[POINTER_ACT_N];
     Taskbar taskbar;
+    Cmd_center cmd_center;
     double main_area_ratio, fixed_area_ratio;
 };
 typedef struct wm_tag WM;
@@ -211,11 +233,11 @@ struct move_info_tag /* 定位器新舊坐標信息 */
 };
 typedef struct move_info_tag Move_info;
 
-struct resize_info_tag /* 調整窗口尺寸的信息 */
+struct delta_rect_tag /* 調整窗口尺寸的信息 */
 {
     int dx, dy, dw, dh; /* 分別爲窗口坐標和尺寸的變化量 */
 };
-typedef struct resize_info_tag Resize_info;
+typedef struct delta_rect_tag Delta_rect;
 
 struct string_format_tag
 {
@@ -234,7 +256,10 @@ int my_x_error_handler(Display *display, XErrorEvent *e);
 void create_font_set(WM *wm);
 void create_cursors(WM *wm);
 void create_taskbar(WM *wm);
+void create_taskbar_buttons(WM *wm);
+void create_icon_area(WM *wm);
 void create_status_area(WM *wm);
+void create_cmd_center(WM *wm);
 void print_fatal_msg(Display *display, XErrorEvent *e);
 void create_clients(WM *wm);
 void *malloc_s(size_t size);
@@ -254,10 +279,11 @@ unsigned int get_num_lock_mask(WM *wm);
 void grab_buttons(WM *wm, Client *c);
 void handle_events(WM *wm);
 void handle_button_press(WM *wm, XEvent *e);
+void focus_clicked_client(WM *wm, Window win);
 bool is_func_click(WM *wm, Widget_type type, Buttonbind *b, XEvent *e);
 bool is_equal_modifier_mask(WM *wm, unsigned int m1, unsigned int m2);
+void choose_client(WM *wm, XEvent *e, Func_arg arg);
 bool is_click_client_in_preview(WM *wm, Widget_type type);
-void choose_client_in_preview(WM *wm, Client *c);
 void handle_config_request(WM *wm, XEvent *e);
 void config_managed_client(WM *wm, Client *c);
 void config_unmanaged_win(WM *wm, XConfigureRequestEvent *e);
@@ -266,6 +292,7 @@ void del_client(WM *wm, Client *c);
 void handle_expose(WM *wm, XEvent *e);
 void update_icon_text(WM *wm, Window win);
 void update_taskbar_button_text(WM *wm, size_t index);
+void update_cmd_center_button_text(WM *wm, size_t index);
 void handle_key_press(WM *wm, XEvent *e);
 unsigned int get_valid_mask(WM *wm, unsigned int mask);
 unsigned int get_modifier_mask(WM *wm, KeySym key_sym);
@@ -279,7 +306,7 @@ void draw_string(WM *wm, Drawable d, const char *str, const String_format *f);
 void get_string_size(WM *wm, const char *str, unsigned int *w, unsigned int *h);
 void exec(WM *wm, XEvent *e, Func_arg arg);
 void key_move_resize_client(WM *wm, XEvent *e, Func_arg arg);
-bool is_valid_move_resize(WM *wm, Client *c, Resize_info *r);
+bool is_valid_move_resize(WM *wm, Client *c, Delta_rect *d);
 void quit_wm(WM *wm, XEvent *e, Func_arg unused);
 void close_client(WM *wm, XEvent *e, Func_arg unused);
 void close_all_clients(WM *wm, XEvent *e, Func_arg unused);
@@ -318,18 +345,20 @@ Rect get_button_rect(Client *c, size_t index);
 void update_title_area_text(WM *wm, Client *c);
 void update_title_button_text(WM *wm, Client *c, size_t index);
 void update_status_area_text(WM *wm);
-void move_resize_client(WM *wm, Client *c, const Resize_info *r);
+void move_resize_client(WM *wm, Client *c, const Delta_rect *d);
 void fix_win_rect_for_frame(Client *c);
 void update_frame(WM *wm, Client *c);
 void update_win_background(WM *wm, Window win, unsigned long color);
 Widget_type get_widget_type(WM *wm, Window win);
 void handle_enter_notify(WM *wm, XEvent *e);
 void hint_enter_taskbar_button(WM *wm, Widget_type type);
+void hint_enter_cmd_center_button(WM *wm, Widget_type type);
 void hint_enter_title_button(WM *wm, Client *c, Widget_type type);
 void hint_resize_client(WM *wm, Client *c, int x, int y);
 bool is_layout_adjust_area(WM *wm, Window win, int x);
 void handle_leave_notify(WM *wm, XEvent *e);
 void hint_leave_taskbar_button(WM *wm, Widget_type type);
+void hint_leave_cmd_center_button(WM *wm, Widget_type type);
 void hint_leave_title_button(WM *wm, Client *c, Widget_type type);
 void maximize_client(WM *wm, XEvent *e, Func_arg unused);
 void iconify(WM *wm, Client *c);
@@ -337,9 +366,8 @@ void create_icon(WM *wm, Client *c);
 void set_icons_rect_for_add(WM *wm, Client *c);
 void move_later_icons(WM *wm, Client *ref, Client *except, int dx);
 bool is_later_icon_client(Client *ref, Client *cmp);
-Client *find_same_class_icon_client(WM *wm, Client *c, int *n);
+Client *find_same_class_icon_client(WM *wm, Client *c, unsigned int *n);
 void set_icon_x_for_add(WM *wm, Client *c);
-void key_choose_client(WM *wm, XEvent *e, Func_arg arg);
 void deiconify(WM *wm, Client *c);
 void del_icon(WM *wm, Client *c);
 void fix_icon_pos_for_preview(WM *wm);
@@ -347,7 +375,7 @@ void update_area_type(WM *wm, Client *c, Area_type type);
 void pointer_move_client(WM *wm, XEvent *e, Func_arg arg);
 void pointer_resize_client(WM *wm, XEvent *e, Func_arg arg);
 Pointer_act get_resize_act(Client *c, const Move_info *m);
-Resize_info get_resize_info(Client *c, const Move_info *m);
+Delta_rect get_delta_rect(Client *c, const Move_info *m);
 void adjust_layout_ratio(WM *wm, XEvent *e, Func_arg arg);
 bool change_layout_ratio(WM *wm, int ox, int nx);
 bool is_main_sec_gap(WM *wm, int x);
@@ -357,5 +385,7 @@ void deiconify_all_clients(WM *wm, XEvent *e, Func_arg arg);
 Client *win_to_iconic_state_client(WM *wm, Window win);
 void change_default_area_type(WM *wm, XEvent *e, Func_arg arg);
 void clear_zombies(int unused);
+void toggle_focus_mode(WM *wm, XEvent *e, Func_arg arg);
+void open_cmd_center(WM *wm, XEvent *e, Func_arg arg);
 
 #endif
