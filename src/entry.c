5d7f4fb2cd426fa85a6c1dffa18a0fef808e6d45
gsm <406643764@qq.com>
2023-07-02 18:57:45 +0800

    * 發布第2.4.3版；         * 修復在進行entry相關操作時內存泄漏的缺陷；         * 修復在運行命令框爲空並回車時錯誤地匹配命令的缺陷。

--- src/entry.c
+++ src/entry.c
@@ -12,8 +12,9 @@
 #include "gwm.h"
 
 static int get_entry_cursor_x(WM *wm, Entry *e);
-static void hint_for_run_cmd_entry(WM *wm, const char *pattern);
-static char *get_match_cmd(const char *pattern);
+static void hint_for_run_cmd_entry(WM *wm, const char *regex);
+static char *get_match_cmd(const char *regex);
+static char *get_part_match_regex(Entry *e);
 static void complete_cmd_for_entry(Entry *e);
 static bool close_entry(WM *wm, Entry *e, bool result);
 
@@ -67,21 +68,20 @@ static int get_entry_cursor_x(WM *wm, Entry *e)
     return get_font_pad(wm, ENTRY_FONT)+w;
 }
 
-static void hint_for_run_cmd_entry(WM *wm, const char *pattern)
+static void hint_for_run_cmd_entry(WM *wm, const char *regex)
 {
     Window win=wm->hint_win;
     char *paths=getenv("PATH");
 
-    if(paths && pattern && *pattern)
+    if(paths && regex && strcmp(regex, "*"))
     {
         int bw=wm->cfg->border_width, w=wm->cfg->run_cmd_entry_width,
             h=ENTRY_HEIGHT(wm), x=wm->run_cmd->x+bw,
             y=wm->run_cmd->y+wm->run_cmd->h+bw, i, n, max=(wm->workarea.h-y)/h;
         String_format fmt={{0, 0, w, h}, CENTER_LEFT, false, true, false, 0,
             TEXT_COLOR(wm, HINT), HINT_FONT};
-        const char *reg=copy_strings(pattern, "*", NULL);
 
-        File *f, *files=get_files_in_paths(paths, reg, RISE, false, &n);
+        File *f, *files=get_files_in_paths(paths, regex, RISE, false, &n);
         if(n)
         {
             XMoveResizeWindow(wm->display, win, x, y, w, MIN(n, max)*h);
@@ -96,16 +96,13 @@ static void hint_for_run_cmd_entry(WM *wm, const char *pattern)
     XUnmapWindow(wm->display, win);
 }
 
-static char *get_match_cmd(const char *pattern)
+static char *get_match_cmd(const char *regex)
 {
     char *cmd=NULL, *paths=getenv("PATH");
-
     if(!paths)
         return NULL;
 
-    char *reg=copy_strings(pattern, "*", NULL);
-    File *files=get_files_in_paths(paths, reg, RISE, false, NULL);
-
+    File *files=get_files_in_paths(paths, regex, RISE, false, NULL);
     if(files->next)
         cmd=copy_string(files->next->name), free_files(files);
     return cmd;
@@ -113,15 +110,15 @@ static char *get_match_cmd(const char *pattern)
 
 bool input_for_entry(WM *wm, Entry *e, XKeyEvent *ke)
 {
-    size_t *i=&e->cursor_offset, no=wcslen(e->text+*i);
-    wchar_t keyname[FILENAME_MAX]={0};
+    wchar_t *s=e->text, keyname[FILENAME_MAX]={0};
+    size_t *i=&e->cursor_offset, no=wcslen(s+*i);
     KeySym ks=look_up_key(e->xic, ke, keyname, FILENAME_MAX);
-    size_t n1=wcslen(e->text), n2=wcslen(keyname), n=ARRAY_NUM(e->text);
+    size_t n1=wcslen(s), n2=wcslen(keyname), n=ARRAY_NUM(e->text);
 
     if(is_equal_modifier_mask(wm, ControlMask, ke->state))
     {
         if(ks == XK_u)
-            wmemmove(e->text, e->text+*i, no+1), *i=0;
+            wmemmove(s, s+*i, no+1), *i=0;
         else if(ks == XK_v)
             XConvertSelection(wm->display, XA_PRIMARY, wm->utf8,
                 None, e->win, ke->time);
@@ -130,45 +127,47 @@ bool input_for_entry(WM *wm, Entry *e, XKeyEvent *ke)
     {
         switch(ks)
         {
-            case XK_Escape:     return close_entry(wm, e, false);
+            case XK_Escape:    return close_entry(wm, e, false);
             case XK_Return:
-            case XK_KP_Enter:   complete_cmd_for_entry(e); return close_entry(wm, e, true);
-            case XK_BackSpace:
-                if(n1)
-                    wmemmove(e->text+*i-1, e->text+*i, no+1), (*i)--;
-                break;
+            case XK_KP_Enter:  complete_cmd_for_entry(e); return close_entry(wm, e, true);
+            case XK_BackSpace: if(n1) wmemmove(s+*i-1, s+*i, no+1), --*i; break;
             case XK_Delete:
-            case XK_KP_Delete:
-                if(n1 < n)
-                    wmemmove(e->text+*i, e->text+*i+1, no+1);
-                break;
+            case XK_KP_Delete: if(n1 < n) wmemmove(s+*i, s+*i+1, no+1); break;
             case XK_Left:
-            case XK_KP_Left:    *i = *i ? *i-1 : *i; break;
+            case XK_KP_Left:   *i = *i ? *i-1 : *i; break;
             case XK_Right:
-            case XK_KP_Right:   *i = *i<n1 ? *i+1 : *i; break;
-            case XK_Home:       (*i)=0; break;
-            case XK_End:        (*i)=n1; break;
-            case XK_Tab:        complete_cmd_for_entry(e); break;
-            default:            wcsncpy(e->text+n1, keyname, n-n1-1), (*i)+=n2;
+            case XK_KP_Right:  *i = *i<n1 ? *i+1 : *i; break;
+            case XK_Home:      (*i)=0; break;
+            case XK_End:       (*i)=n1; break;
+            case XK_Tab:       complete_cmd_for_entry(e); break;
+            default:           wcsncpy(s+n1, keyname, n-n1-1), (*i)+=n2;
         }
     }
     else if(is_equal_modifier_mask(wm, ShiftMask, ke->state))
-        wcsncpy(e->text+n1, keyname, n-n1-1), (*i)+=n2;
+        wcsncpy(s+n1, keyname, n-n1-1), (*i)+=n2;
 
-    char pattern[FILENAME_MAX]={0};
-    wcstombs(pattern, e->text, FILENAME_MAX);
-    hint_for_run_cmd_entry(wm, pattern);
+    char *regex=get_part_match_regex(e);
+    hint_for_run_cmd_entry(wm, regex), free(regex);
     update_entry_text(wm, e);
     return false;
 }
 
 static void complete_cmd_for_entry(Entry *e)
 {
-    char *cmd, pattern[FILENAME_MAX]={0};
+    if(!e->text[0])
+       return;
 
-    wcstombs(pattern, e->text, FILENAME_MAX);
-    if((cmd=get_match_cmd(pattern)))
+    char *regex=get_part_match_regex(e), *cmd=get_match_cmd(regex);
+    if(cmd)
         mbstowcs(e->text, cmd, FILENAME_MAX), e->cursor_offset=wcslen(e->text);
+    vfree(regex, cmd, NULL);
+}
+
+static char *get_part_match_regex(Entry *e)
+{
+    char text[FILENAME_MAX]={0};
+    wcstombs(text, e->text, FILENAME_MAX);
+    return copy_strings(text, "*", NULL);
 }
 
 static bool close_entry(WM *wm, Entry *e, bool result)
